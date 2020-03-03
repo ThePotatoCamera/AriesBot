6166f4336441bbadeba468caad203dc4b563dd22
PokeToribio <PokeToribio@hotmail.com>
2020-03-03 15:57:20 +0100

    Actu servidores

--- commands/servidores.js
+++ commands/servidores.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
 
-message.channel.send(`Dando servicio a ${bot.guilds.size} servidores.`);
+message.channel.send(`Dando servicio a ${client.guilds.size} servidores con un total de ${client.users.size} usuarios.`);
 
 }
 
