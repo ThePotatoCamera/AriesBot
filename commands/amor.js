fe25865d41690b832a11b236dae7b179cfbb5cdd
ThePotatoCamera <32365704+ThePotatoCamera@users.noreply.github.com>
2020-04-09 00:29:32 +0200

    Fix a #7

--- commands/amor.js
+++ commands/amor.js
@@ -1,49 +1,75 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {    
-    let users = message.mentions.users.map(m => m.username).join(' y ');
-    if(!users) return message.channel.send('porfavor menciona a 2 personas');
-    const random = Math.floor(Math.random() * 101);
-
-    let heard = random;
+    const mention = message.mentions.users.first()
+    if(!mention) return message.channel.send('Por favor, menciona a alguien.');
+    if(message.author == mention) {
+        random = 200;
+    }
+    else {
+        random = Math.floor(Math.random() * 100);
+    }
+    
+    let hearth = random;
  
-    if(heard < 50){
-        
-        heard='La tension se corta con un chchillo';
+    if (hearth < 20) {
+        hearth=':scream: ¡Cuidado que vuelan cuchillos! :scream:';
 
-    }else if(heard < 30){
-        heard=':scream: Cuidado que vuelvan cuchillos :scream:';
+    }
+    else if (hearth < 50) {
+        hearth = 'Hay cierta tensión en el ambiente...'
+    }
+    else if (hearth == 100) {
+        hearth = '¡Es amor verdadero!'
+    }
+    else if (hearth == 200) {
+        hearth = `${message.author} se ama demasiado a si mismo.`
+    }
+    else if (hearth > 90) {
+        hearth='¡El amor flota en el ambiente!';
 
-    }else if(heard < 80){
-        heard='El amor se empieza a manifestar';
+    }
+    else if (hearth >= 75) {
+        hearth='El amor se empieza a manifestar.';
         
-    }else if(heard < 101){
-        heard='El amor flota en el amienté!';
+    }
+    else if (hearth >= 50) {
+        
+        hearth='Aquí solo hay amistad, despejen.';
 
     }
 
-    let heard2 = "";
+    let hearth2 = "";
  
-    if(random < 30){
-        heard2=':broken_heart: ';
-
-    }else if(random < 50){
-        heard2=':yellow_heart:';
-
-    }else if(random < 80){
-        heard2=':cupid: ';
-        
-    }else if(random < 105){
-        heard2=':sparkling_heart: ';
+    if (random < 20) {
+        hearth2 = ':knife:'
+    }
+    else if(random < 50) {
+        hearth2=':broken_heart: ';
 
     }
+    else if (random == 100) {
+        hearth2=':couplekiss:'
+    }
+    else if (random == 200) {
+        hearth2 = ':princess:'
+    }
+    else if(random >= 90) {
+        hearth2=':sparkling_heart: ';
+    }
+    else if(random >= 75) {
+        hearth2=':cupid: ';   
+    }
+    else if(random >= 50) {
+        hearth2=':yellow_heart:';
+    } 
+
 
     let ballembed = new Discord.RichEmbed()
     .setColor("#efb810")
-    .setDescription(`*La compatibilidad de* **${users}**`)
-    .addField(`**Es del:**`,`** ${random}% **`)
-    .addField(`*Corazon:*   ${heard2}`, `**${heard}**`)
-    .setFooter(`Mensaje enviado`)
+    .setDescription(`La compatibilidad de **${message.author.username}** con **${mention.username}**`)
+    .addField(`**Es del**`,`** ${random}% **`)
+    .addField(`*Corazón:*   ${hearth2}`, `**${hearth}**`)
     .setTimestamp()
     message.channel.send(ballembed)
       
