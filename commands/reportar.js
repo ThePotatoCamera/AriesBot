12d883f2a0c718d7b0f1d242622a5dd3f7d58d31
ThePotatoCamera <32365704+ThePotatoCamera@users.noreply.github.com>
2020-05-04 17:52:39 +0200

    Optimizaciones

--- commands/reportar.js
+++ commands/reportar.js
@@ -1,21 +1,22 @@
 exports.run = (client, message, args) => {
-    const Discord = require('discord.js');
-    const fs = require(`fs`);
-    const botconfig = require("../botconfig.json");
     
     let reporte = args.slice(0).join(" ")
 
-    if (!args[0]) return message.channel.send("Porfavor añade el reporte en forma de mensaje")
+    if (!args[0]) return message.author.send("Por favor, añade el reporte en forma de mensaje.")
+    .then (message.delete());
     
+    message.delete();
     client.guilds.get(`678282402979905547`).client.channels.get('681147632340566055').send({embed: {
         color: 3447003,
         description: reporte,
         timestamp: new Date(),
         footer: {
             icon_url: message.author.avatarURL,
-            text: `Mensaje enviado por: ${message.author.tag}`
+            text: `Mensaje enviado por: ${message.author.tag} (${message.author.id})`
           }
-      }}).then(message.channel.send(`Reporte enviado, recuerda que puedes unirte al servidor de soporte con **d!soporte**`))
+      }}).then(
+          message.author.send(`Reporte enviado, recuerda que puedes unirte al servidor de soporte con **d!soporte**.`)
+      )
 };
 module.exports.help = {
     name: "reportar"
