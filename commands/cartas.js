d45edc327b923c3497914937e9ea91276a133c20
ThePotatoCamera <32365704+ThePotatoCamera@users.noreply.github.com>
2020-06-22 22:42:50 +0200

    Assets cambiados a imgur

--- commands/cartas.js
+++ commands/cartas.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
  
     if (message.guild) {
         {
-            const images = ["https://i.ibb.co/64sQDBm/Photo-Grid-1556127249386.jpg", "https://i.ibb.co/vL9PBqM/Photo-Grid-1555617543254.jpg","https://i.ibb.co/DYTQ9xY/Photo-Grid-1555615124019.jpg","https://i.ibb.co/TqjYwMf/Photo-Grid-1555614914103.jpg","https://i.ibb.co/C0hsqCh/Photo-Grid-1555614771591.jpg","https://i.ibb.co/BPcsyXz/Photo-Grid-1555614438905.jpg","https://i.ibb.co/M57NWTL/Photo-Grid-1555439489124.jpg","https://i.ibb.co/wWGtyvL/Photo-Grid-1555437299273.jpg","https://i.ibb.co/SvCvLsm/IMG-20190424-195451.jpg","https://i.ibb.co/C6sCpc9/IMG-20190424-194435.jpg","https://i.ibb.co/DKHr9X8/IMG-20190416-192611.jpg"];
+            const images = ["https://i.imgur.com/TuV5wSO.jpg", "https://i.imgur.com/nq7LiZi.jpg","https://i.imgur.com/j4E2WF3.jpg","https://i.imgur.com/LcjzGwy.jpg","https://i.imgur.com/w0NJFxF.jpg","https://i.imgur.com/ye6GrYh.jpg","https://i.imgur.com/9lPQyca.jpg","https://i.imgur.com/Go492WO.jpg","https://i.imgur.com/nrFvRzL.jpg","https://i.imgur.com/uHwcZmV.jpg","https://i.imgur.com/Gs3RD6x.jpg"];
             const embed = new Discord.MessageEmbed()
                 .setColor(0xefb810)
                 .setDescription(`${message.author} ha sacado la carta:`)
