6abf779e858d4adda3407b175d8e75b90b550f8c
ThePotatoCamera <32365704+ThePotatoCamera@users.noreply.github.com>
2020-06-23 13:03:57 +0200

    fixeo a mencion

--- commands/trucha.js
+++ commands/trucha.js
@@ -4,15 +4,10 @@ exports.run = (client, message, args) => {
 	if (message.guild) {
         const images = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Rainbow_trout_transparent.png/200px-Rainbow_trout_transparent.png';
 		const mention = message.mentions.members.first();
-		if (message.author == mention || mention.id == '323221933980123136') {
-            return;
-		}
-		else if (args < 1) {
-            return;
-        }
+		if (message.author == mention) return;
+		else if (args < 1) return;
+		else if (mention == '323221933980123136') return;
 		else {
-			let user = message.mentions.members.first()
-      if (!user) return message.reply("Por favor, menciona a alguien");
 			const embed = new Discord.MessageEmbed()
 				.setColor(0xefb810)
 				.setDescription(`${mention} ha recibido un truchazo de n00b.`)
