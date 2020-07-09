abd8c122335648f4b3f1a79575cea81f2d48c779
ThePotatoCamera <32365704+ThePotatoCamera@users.noreply.github.com>
2020-07-10 01:17:34 +0200

    Fix a #21 (#32)

--- commands/reiniciar.js
+++ commands/reiniciar.js
@@ -1,16 +1,20 @@
 module.exports.run = (client, message, args) => {
 
-	const server = client.guilds.get('678282402979905547');
-    const staff = server.roles.get('681152401109024781').id;
-	   if (message.member.roles.some(r => r.id == staff)) {
+	const server = client.guilds.cache.get('678282402979905547');
+	const staff = server.roles.cache.get('681152401109024781');
+	
+	   if (staff.members.get(message.author.id)) {
+
 		console.log(`${message.author.tag} (${message.author.id}) ha reiniciado el bot.`)
 		message.channel.send('Reiniciando Aries...')
 		client.destroy();
 		client.login(process.env.TOKEN)
 		.then(() => message.reply("¡Bot reiniciado!"));
+		
 	   }
 	   else return message.channel.send("Comando disponible solo para el staff de Aries Bot.")
 	}
+
 module.exports.help = {
     name: "reiniciar"
   }
