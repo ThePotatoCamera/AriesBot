f5328354cdac8b62c60ee2259bfcb5145d704b82
ThePotatoCamera <32365704+ThePotatoCamera@users.noreply.github.com>
2020-07-17 01:43:58 +0200

    Fix a #35
    Se me habría escapado con tantos strings que tengo que revisar.

--- commands/staff.js
+++ commands/staff.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
  const embed = new Discord.MessageEmbed() 
-.setTitle("Aries es posible a estas personas:")
+.setTitle("Aries es posible gracias a estas personas:")
 .setColor(0x00AE86)
 .addField("PokeToribio","Dev <:Verificado:681246902636904450>") 
 .addField("MarisaTheKiller","Diseñadora y ayudante <:Verificado:681246902636904450>")  
