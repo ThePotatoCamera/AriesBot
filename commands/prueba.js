a761e8e89fc21b7a24b84f2099e41fc484dd4511
PokeToribio <43357212+PokeToribio@users.noreply.github.com>
2020-10-22 17:46:32 +0200

    Primer commit

---
+++ commands/prueba.js
@@ -0,0 +1,8 @@
+exports.run = (client, message, args) => {
+	const Discord = require('discord.js');
+
+message.channel.send("Mensaje de prueba")
+};
+module.exports.help = {
+    name: "prueba"
+  }
