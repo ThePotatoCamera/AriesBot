ccfc740023f6e766847929c7ae76fb4de79917ea
PokeToribio <43357212+PokeToribio@users.noreply.github.com>
2021-04-30 13:54:42 +0200

    Arreglado elegir

--- commands/elegir.js
+++ commands/elegir.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!args) return message.channel.send("¿Qué desea preguntarme?")
+    if(args == 0) return message.channel.send("¿Qué desea preguntarme?")
     if(args.length > 10) return message.channel.send('Lo siento, solo puedo elegir entre 10 opciones.')
     let question = args
     let replies = args
