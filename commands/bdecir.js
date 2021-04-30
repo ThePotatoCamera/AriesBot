97ee68aa3fe58bdedc62e483fb7bf3f88b19e0a7
PokeToribio <43357212+PokeToribio@users.noreply.github.com>
2021-04-30 13:13:22 +0200

    Arreglado bug de everyone/here

--- commands/bdecir.js
+++ commands/bdecir.js
@@ -4,6 +4,8 @@ module.exports.run = async (bot, message, args) => {
 
     if(message.channel.name == undefined) return message.channel.send("No puedes ejecutar este comando en un canal privado.")
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No tienes permisos.");
+    if(message.content.includes("@everyone")) return message.channel.send("No intentes abusar del everyone")
+    if(message.content.includes("here")) return message.channel.send("No intentes abusar del here")
     let bdecir = args.slice(0).join(" ")
     if(!bdecir) return message.channel.send("Que quieres que diga?")
     message.channel.bulkDelete(1).then(msg => msg.delete(2))
--- commands/decir.js
+++ commands/decir.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     if(message.channel.name == undefined) return message.channel.send("No puedes ejecutar este comando en un canal privado.")
+    if(message.content.includes("@everyone")) return message.channel.send("No intentes abusar del everyone")
+    if(message.content.includes("here")) return message.channel.send("No intentes abusar del here")
     if(!args[0]) return message.channel.send("Debes poner que quieres que diga.")
     let decir = args.slice(0).join(" ")
     message.channel.send(decir)
