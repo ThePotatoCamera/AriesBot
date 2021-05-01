872c666254ad95768ae29d22de1133371a83e723
PokeToribio <43357212+PokeToribio@users.noreply.github.com>
2021-05-01 16:30:10 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -14,6 +14,6 @@ exports.run = async (bot) => {
 
     delete require.cache[require.resolve('../commands/recargar.js')];
     console.log('[BOOT] Se ha recargado el comando de reiniciar.');
-    console.log(`[INFO] Bot cargado con éxito, ejecutando versión ${config.longVersion}. Sirviendo a ${bot.guilds.cache.size} servidores.`);
+    console.log(`[INFO] Bot cargado con éxito, ejecutando versión ${process.env.longVersion}. Sirviendo a ${bot.guilds.cache.size} servidores.`);
     console.log(`[INFO] Prefix predeterminado: ${process.env.PREFIX}`);
 };
