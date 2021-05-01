5ca7be5c271eb8462c12aa9c8065b9090433c7be
PokeToribio <43357212+PokeToribio@users.noreply.github.com>
2021-05-01 15:10:12 +0200

    pre-arreglo a #47

--- commands/usuario.js
+++ commands/usuario.js
@@ -3,14 +3,22 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     if(message.channel.name == undefined) return message.channel.send("No puedes ejecutar este comando en un canal privado.")
-    let miembro = message.mentions.users.first()
+    let miembro = message.mentions.members.first()
     if(!miembro) return message.channel.send("Debes mencionar a alguien.")
+
+    let estados ={
+      "online": "Conectado",
+      "idle": "Ausente",
+      "dnd": "No molestar",
+      "offline": "Desconectado/invisible"
+    };
+
   const embed = new Discord.MessageEmbed()
     .addField("***Información de usuario***", miembro.tag)
     .setThumbnail(miembro.avatarURL)
     .addField('ID:', miembro.id, false)
     .addField('Cuenta creada el:', miembro.createdAt)
-    .addField("Presencia:", miembro.presence.status, true)
+    .addField("Presencia:", estados[miembro.user.presence.status], true)
     .setTimestamp()
     .setColor("#FF9900")
     
