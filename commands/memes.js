cdfc40d71904146aca82a17dad150499ebbc7f4b
PokeToribio <43357212+PokeToribio@users.noreply.github.com>
2021-05-07 20:56:41 +0200

    Pedido por potato

---
+++ commands/memes.js
@@ -0,0 +1,28 @@
+exports.run = async (client, message, args) => {
+    const Discord = require('discord.js');
+    const api = require("imageapi.js")
+
+    let reddit = ["AnimeFunny","memes","dankmemes","animemes","SpanishMeme","MemesEnEspanol"]
+    let imagenreddit = reddit[Math.floor(Math.random()*(reddit.length))]
+    let img = await api(imagenreddit)
+
+    const embed = new Discord.MessageEmbed()
+                .setTitle(`Meme sacado de r/${imagenreddit}`)
+                .setURL(`https://reddit.com/r/${imagenreddit}`)
+                .setColor(0xefb810)
+                .setImage(img)
+            message.channel.send({ embed }).catch(err => {
+                if (err == "Image was marked as NSFW") {
+                  console.log("Error memes.js")
+                }
+                else {
+                console.error(err)
+                }
+              })
+}
+.catch(err => {
+  console.error(err)
+})
+module.exports.help = {
+    name: "memes"
+  }
