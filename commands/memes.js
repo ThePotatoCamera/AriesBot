0e8399563d5936d77168c798a5da8585254110bd
ThePotatoCamera <alex0xela@outlook.com>
2021-08-22 23:57:06 +0200

    Fix a que poke no sabe que el error es de la API (#63)

--- commands/memes.js
+++ commands/memes.js
@@ -2,24 +2,18 @@ module.exports.run = async (client, message, args) => {
   const Discord = require('discord.js');
   const api = require("imageapi.js")
 
-  //let reddit = ["AnimeFunny","memes","dankmemes","animemes","SpanishMeme","MemesEnEspanol"]
-  let reddit = ["sex_comics"] //puesto solo para ver que pasaria si se encuentra con cosas NSFW, quitar para el normal
-  let imagenreddit = reddit[Math.floor(Math.random()*(reddit.length))]
-  let img = await api(imagenreddit)
+  const reddit = ["MAAU","SpanishMeme","MemesEnEspanol","orslokx","dylanteroyt"]
+  const imagenreddit = reddit[Math.floor(Math.random()*(reddit.length))]
+  const img = await api(imagenreddit).catch(e => {
+    console.error('[ERROR API REDDIT]' + e)
+  })
 
   const embed = new Discord.MessageEmbed()
               .setTitle(`Meme sacado de r/${imagenreddit}`)
               .setURL(`https://reddit.com/r/${imagenreddit}`)
               .setColor(0xefb810)
               .setImage(img)
-          message.channel.send({ embed }).catch(err => {
-              if (err == "Image was marked as NSFW") {
-                console.log("Error NSFW memes")
-              }
-              else {
-              console.error(err)
-              }
-            })
+  message.channel.send({ embed })
 }
 
 module.exports.help = {
