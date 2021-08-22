e4ca792a6b7aa3dd5405a5df5d5ae7ce73e6f7a6
PokeToribio <43357212+PokeToribio@users.noreply.github.com>
2021-08-22 21:19:15 +0200

    #63

--- commands/memes.js
+++ commands/memes.js
@@ -1,26 +1,27 @@
 module.exports.run = async (client, message, args) => {
-    const Discord = require('discord.js');
-    const api = require("imageapi.js")
+  const Discord = require('discord.js');
+  const api = require("imageapi.js")
 
-    let reddit = ["AnimeFunny","memes","dankmemes","animemes","SpanishMeme","MemesEnEspanol"]
-    let imagenreddit = reddit[Math.floor(Math.random()*(reddit.length))]
-    let img = await api(imagenreddit)
+  //let reddit = ["AnimeFunny","memes","dankmemes","animemes","SpanishMeme","MemesEnEspanol"]
+  let reddit = ["sex_comics"] //puesto solo para ver que pasaria si se encuentra con cosas NSFW, quitar para el normal
+  let imagenreddit = reddit[Math.floor(Math.random()*(reddit.length))]
+  let img = await api(imagenreddit)
 
-    const embed = new Discord.MessageEmbed()
-                .setTitle(`Meme sacado de r/${imagenreddit}`)
-                .setURL(`https://reddit.com/r/${imagenreddit}`)
-                .setColor(0xefb810)
-                .setImage(img)
-            message.channel.send({ embed }).catch(err => {
-                if (err == "Image was marked as NSFW") {
-                  console.log("Error memes.js")
-                }
-                else {
-                console.error(err)
-                }
-              })
+  const embed = new Discord.MessageEmbed()
+              .setTitle(`Meme sacado de r/${imagenreddit}`)
+              .setURL(`https://reddit.com/r/${imagenreddit}`)
+              .setColor(0xefb810)
+              .setImage(img)
+          message.channel.send({ embed }).catch(err => {
+              if (err == "Image was marked as NSFW") {
+                console.log("Error NSFW memes")
+              }
+              else {
+              console.error(err)
+              }
+            })
 }
 
 module.exports.help = {
-    name: "memes"
-  }
+  name: "memes"
+}
